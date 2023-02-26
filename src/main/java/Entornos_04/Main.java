8900d43b9f75f8013d1fdebbb70d1e157ff558f3
evavg <evavg@LAPTOP-0PQ45POA.fritz.box>
2023-02-26 18:12:09 +0100

    Carga inicial

--- src/main/java/Entornos_04/Main.java
+++ src/main/java/Entornos_04/Main.java
@@ -2,6 +2,8 @@ package Entornos_04;
 
 public class Main {
 
+    private static float nf;
+
     public static void main(String[] args) {
         
         CCuenta cuenta1;
@@ -14,6 +16,7 @@ public class Main {
     private static void operativa_cuenta(float cantidad) {
         CCuenta cuenta1;
         double saldoActual;
+        
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es" + saldoActual );
